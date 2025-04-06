b9d524adc832d737591b2b06ce8505717c7be6de
Maximilian Schöpf <172821797+Peter-Pander@users.noreply.github.com>
2025-04-07 00:44:37 +0200

    Add tests for Task#truncated_description method

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  describe '#initialize' do
-    let(:task) { Task.new(title: 'Sample Task Title', description: 'This is a sample task description used for testing purposes.') }
+  let(:task) { Task.new(title: 'Sample Task Title', description: 'This is a sample task description used for testing purposes.') }
 
+  describe '#initialize' do
     it 'is valid with all columns present' do
       expect(task.valid?).to eq(true)
     end
@@ -35,6 +35,25 @@ RSpec.describe Task, type: :model do
       it 'returns error message' do
         task.valid?
         expect(task.errors.messages).to eq({ description: ["can't be blank"]})
+      end
+    end
+  end
+
+  describe '#truncated_description' do
+    context 'with long description' do
+      it 'returns truncated description' do
+        # "This is a sample task description used for testing purposes." truncated to first 11 characters, with an ellipsis appended, becomes "This is a s..."
+        expect(task.truncated_description).to eq('This is a s...')
+      end
+    end
+
+    context 'with short description' do
+      before do
+        task.description = 'Brief'
+      end
+
+      it 'returns full description' do
+        expect(task.truncated_description).to eq('Brief')
       end
     end
   end
